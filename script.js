7aacab6b0687244f69244df886d2fd6b108f38dd
Gonzlezjg <15gonzalez.j.p@gmail.com>
2020-10-24 21:26:23 -0400

    Upload article

---
+++ script.js
@@ -0,0 +1,8 @@
+ const abrir = document.getElementById('share');
+ const button = document.getElementById('button');
+
+  button.addEventListener('click', () => {
+      abrir.classList.toggle('btn-share')
+  })
+    
+
